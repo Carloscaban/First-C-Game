ecb4fceb4d7f6a9801589daf2802eb458132ee84
CARLOS CABAN GONZALEZ <carlos.caban10@upr.edu>
2023-12-26 21:45:43 -0400

    Drawing

--- FirstC++Game/FirstC++Game/Win32Platform.cpp
+++ FirstC++Game/FirstC++Game/Win32Platform.cpp
@@ -1,6 +1,19 @@
+#include "utils.cpp"
 #include <windows.h>
 
-bool running = true;
+
+globalVariable bool running = true;
+
+struct RenderState {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmapInfo;
+};
+
+globalVariable RenderState renderState;
+
+#include "renderer.cpp"
 
 LRESULT CALLBACK windowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -14,11 +27,22 @@ LRESULT CALLBACK windowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 		case WM_SIZE: {
 			RECT rect;
 			GetClientRect(hwnd, &rect);
-			int width = rect.right - rect.left;
-			int height = rect.bottom - rect.top;
+			renderState.width = rect.right - rect.left;
+			renderState.height = rect.bottom - rect.top;
 
-			int bufferSiz = width * height * sizeof(unsigned int);
-		}
+			int size = renderState.width * renderState.height * sizeof(unsigned int);
+
+			if (renderState.memory) VirtualFree(renderState.memory, 0, MEM_RELEASE);
+			renderState.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			renderState.bitmapInfo.bmiHeader.biSize = sizeof(renderState.bitmapInfo.bmiHeader);
+			renderState.bitmapInfo.bmiHeader.biWidth = renderState.width;
+			renderState.bitmapInfo.bmiHeader.biHeight = renderState.height;
+			renderState.bitmapInfo.bmiHeader.biPlanes = 1;
+			renderState.bitmapInfo.bmiHeader.biBitCount = 32;
+			renderState.bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+		} break;
 
 		default: {
 			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -40,6 +64,8 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	// Create Window
 	HWND window = CreateWindow(windowClass.lpszClassName, "My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
+
 
 	while (running) {
 		// input
@@ -50,10 +76,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		}
 
 		// Simulate
-
+		clearScreen(0xff5500);
+		drawRect(0, 0, 1, 1, 0x00ff22);
 
 		// Render
-
-
+		StretchDIBits(hdc, 0, 0, renderState.width, renderState.height, 0, 0, renderState.width, renderState.height, renderState.memory, &renderState.bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
---
+++ FirstC++Game/FirstC++Game/renderer.cpp
@@ -0,0 +1,49 @@
+internal void
+clearScreen(u32 color) {
+	u32* pixel = (u32*)renderState.memory;
+
+	for (int y = 0; y < renderState.height; y++) {
+		for (int x = 0; x < renderState.width; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+internal void
+drawRectInPixels(int x0, int y0, int x1, int y1, u32 color) {
+	
+	// Setting limits so the window does not crash
+	x0 = clamp(0, x0, renderState.width);
+	x1 = clamp(0, x1, renderState.width);
+	y0 = clamp(0, y0, renderState.height);
+	y1 = clamp(0, y1, renderState.height);
+
+	for (int y = y0; y < y1; y++) {
+		u32* pixel = (u32*)renderState.memory + x0 + y*renderState.width;
+		for (int x = x0; x < x1; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+globalVariable float renderScale = 0.01f;
+
+internal void
+drawRect(float x, float y, float halfSizeX, float halfSizeY, u32 color) {
+	x *= renderState.height * renderScale;
+	y *= renderState.height * renderScale;
+	halfSizeX *= renderState.height * renderScale;
+	halfSizeY *= renderState.height * renderScale;
+
+	x += renderState.width / 2.f;
+	y += renderState.height / 2.f;
+
+
+	// change to pixels
+	int x0 = x - halfSizeX;
+	int x1 = x + halfSizeX;
+	int y0 = y - halfSizeY;
+	int y1 = y + halfSizeY;
+
+	drawRectInPixels(x0, y0, x1, y1, color);
+}
